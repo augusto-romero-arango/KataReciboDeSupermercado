e6864d243e8121316badf1d45cd0dad606cd7d48
david.cubides <david.cubides@sinco.com.co>
2025-11-10 12:02:28 -0500

    🟢CadaProductoDebeTenerUnSubtotal_IgualAlPrecioPorCantidad

--- ReciboDeSupermercado.Test/Producto.cs
+++ ReciboDeSupermercado.Test/Producto.cs
@@ -5,5 +5,5 @@ public class Producto
     public string Nombre { get; set; }
     public decimal Precio { get; set; }
     public int Cantidad { get; set; } = 1;
-    public object Subtotal { get; set; }
+    public object Subtotal => Cantidad * Precio;
 }
